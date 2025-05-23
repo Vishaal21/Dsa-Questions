b0a144a7052441e057965cec6eb432b7f4578e3d
Vishal <singhvishal1821@gmail.com>
2025-05-23 21:58:34 +0530

    done with class and objects

---
+++ class4/class_object.py
@@ -0,0 +1,29 @@
+
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def say_hello(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# # Create objects
+# student1 = Student("Alice", 20)
+# student2 = Student("Bob", 22)
+
+# # Call method on objects
+# print(student1.name)
+# student1.say_hello()
+
+# print(student2.age)
+# student2.say_hello()
+
+
+
+a = [1,2,3]
+b = a
+b[0] = 4
+print(a)
+print(b)
+
+
